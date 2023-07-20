c77a654037afa615ae7242995d9047e630eb15f9
James Jordan <james.r.jordan1993@hotmail.com>
2023-07-20 16:37:19 +0100

    add header components

---
+++ my-cv/src/app/Components/header/Components/main-header/main-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-header',
+  templateUrl: './main-header.component.html',
+  styleUrls: ['./main-header.component.css']
+})
+export class MainHeaderComponent {
+  userName:string = 'James Jordan CV';
+  jobTitle:string = 'Junior Software Developer'
+}
--- my-cv/src/app/app.module.ts
+++ my-cv/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { ContactDetailsComponent } from './Components/Header/Components/contact-details/contact-details.component';
+import { MainHeaderComponent } from './Components/Header/Components/main-header/main-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContactDetailsComponent,
+    MainHeaderComponent
   ],
   imports: [
     BrowserModule,
